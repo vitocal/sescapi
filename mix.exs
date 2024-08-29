0b95abd6dabed39fcddeef83742aea737856bf8a
Vitor Calejuri <vitor.cajuleri@gmail.com>
2024-08-29 18:18:10 -0300

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,37 @@
+defmodule Sescapi.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :sescapi,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :test,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_utils, "~> 0.1.7"},
+      {:plug, "~> 1.0"},
+      {:req, "~> 0.5.0"},
+      {:kino, "~> 0.12.0"}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support", "test/fixtures"]
+  defp elixirc_paths(_), do: ["lib"]
+end
